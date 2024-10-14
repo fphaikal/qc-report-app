b2b6effcde36cd8577be6fd822f79b23994dac71
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-10-14 09:29:28 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from "next/link";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 
 export default function Home() {
